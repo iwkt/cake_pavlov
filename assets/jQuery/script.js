79a2b75f8247f6e112e459f2cf73ceaa85ccc5dd
iwkt <iwk@mac.com>
2020-06-19 06:45:46 +0900

    202006190645

--- assets/jQuery/script.js
+++ assets/jQuery/script.js
@@ -7,6 +7,20 @@ document.addEventListener('DOMContentLoaded',function() {
 
 window.addEventListener("scroll", function () {
   let scroll = document.documentElement.scrollTop;
+
+  console.log(scroll);
+
+  if (scroll > 250) {
+    document.querySelector(".bnr-2").classList.add("active");
+  } else {
+    document.querySelector(".bnr-2").classList.remove("active")
+  }
+
+  if (scroll > 670) {
+    document.querySelector(".bnr-3").classList.add("active");
+  } else {
+    document.querySelector(".bnr-3").classList.remove("active")
+  }
   
   if(scroll > 600) {
     document.querySelector("#innerLogo").classList.add("active");
